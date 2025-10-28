6532d1d17a60cd02490a675415f5ca1958583579
Mickael Joseph <mickaelandriana06@gmail.com>
2025-10-28 12:03:10 +0300

    filename extract and execute command

--- convert.sh
+++ convert.sh
@@ -10,3 +10,14 @@ else
     echo "$filename is not valid"
     exit 1
 fi
+
+filenameConvert=$(basename "$filename")
+filenameConvert=${filenameConvert%.*}
+
+echo "===========================Converter start==========================="
+ffmpeg -y -i "$filename"\
+    -r 20 -s 352x288 -vb 400k \
+    -acodec aac -b:a 48k -ac 1 -ar 44100 \
+    "$filenameConvert.3gp"
+
+echo "===========================Converter finish==========================="
