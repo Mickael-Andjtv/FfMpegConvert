3722a1a163de08247e21bb67c34687a3a733ae66
Mickael Joseph <mickaelandriana06@gmail.com>
2025-10-28 11:52:51 +0300

    check if it's a valid file

--- convert.sh
+++ convert.sh
@@ -3,3 +3,10 @@ echo "WELCOME TO MP4 CONVERTER";
 echo -n "enter the path of video >> ";
 read -r filename;
 echo "$filename";
+
+if [[ -f $filename ]]; then
+    echo "$filename is a file"
+else
+    echo "$filename is not valid"
+    exit 1
+fi
